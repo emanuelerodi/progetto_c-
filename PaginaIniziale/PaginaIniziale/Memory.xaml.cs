80f34e4ad2f9e0c6b4095ea8a459df12250c6730
marcoSPADA8 <matzu2050@gmail.com>
2025-12-30 16:20:26 +0100

    pag iniziale + memory
    aggiunta delle immagini, più creazione di bottone che ti manda alla finestra del memory e bottone back to home

--- PaginaIniziale/PaginaIniziale/Memory.xaml.cs
+++ PaginaIniziale/PaginaIniziale/Memory.xaml.cs
@@ -10,25 +10,25 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PaginaIniziale
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logica di interazione per Memory.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Memory : Window
     {
-        public MainWindow()
+        public Memory()
         {
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Memory finestraMemory = new Memory(); 
-            finestraMemory.Show();
+            MainWindow home = new MainWindow(); 
+            home.Show(); 
+
             this.Close();
         }
     }
