e7bc69eb2477d1fb324ee79cad7545e13f7baa34
marcoSPADA8 <matzu2050@gmail.com>
2026-01-12 08:36:15 +0100

    sistemazione errore

--- PaginaIniziale/PaginaIniziale/Tris.xaml.cs
+++ PaginaIniziale/PaginaIniziale/Tris.xaml.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PaginaIniziale
+{
+    /// <summary>
+    /// Logica di interazione per Tris.xaml
+    /// </summary>
+    public partial class Tris : Window
+    {
+        public Tris()
+        {
+            InitializeComponent();
+        }
+    }
+}
